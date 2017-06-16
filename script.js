cdbe4eef91216dc3d20c773a440e133df45da10a
Felipe Cortez <felipecortezfi@gmail.com>
2017-06-15 21:08:56 -0300

    Layout, formatting, fixed middle C

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ var div = document.getElementById("boo")
 var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Configure the rendering context.
-renderer.resize(500, 120);
+renderer.resize(300, 120);
 var context = renderer.getContext();
 context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(0, 0, 400).addClef("treble");
+var stave = new VF.Stave(0, 0, 200).addClef("treble");
 
 // Connect it to the rendering context and draw!
 stave.setContext(context).draw();
@@ -65,14 +65,26 @@ function midiNote(noteValue) {
             "Bb",
             "B"];
 
-    note = {midi: noteValue, pitch: notes[noteValue % 12], octave: Math.floor(noteValue / 12) - 2};
+    note = {midi: noteValue, pitch: notes[noteValue % 12], octave: Math.floor(noteValue / 12)};
     note.string = note.pitch + note.octave;
-    note.vex_string = note.pitch.toLowerCase() + "/" + (note.octave + 1);
+    note.vex_string = note.pitch.toLowerCase() + "/" + (note.octave);
     note.acc = note.pitch[1];
     return note;
 }
 
 notesHeld = [];
+
+/*
+ * TODO
+function getInversion(intervalList) {
+    inversion = {intervals: [4, 3],
+                 name: "major",
+                 root: 0}
+    inversion = {intervals: [3, 5],
+                 name: "major (1st inversion)",
+                 root: 0}
+}
+*/
 
 function inversion(notesHeld) {
     notesList = notesHeld.sort().map(midiNote)
@@ -90,7 +102,17 @@ function inversion(notesHeld) {
         return notesList[2].pitch + " minor (1st inversion)";
     } else if(intervalList.toString() == [5, 3].toString()) {
         return notesList[1].pitch + " minor (2nd inversion)";
+    } else if(intervalList.toString() == [3, 3].toString()) {
+        return notesList[0].pitch + " diminished";
+    } else if(intervalList.toString() == [3, 6].toString()) {
+        return notesList[1].pitch + " diminished (1st inversion)";
+    } else if(intervalList.toString() == [6, 3].toString()) {
+        return notesList[2].pitch + " diminished (2nd inversion)";
+    } else if(intervalList.toString() == [4, 4].toString()) {
+        return notesList[0].pitch + " augmented";
     }
+
+    return "-";
 }
 
 function intervals(notesHeld) {
@@ -112,10 +134,10 @@ function detectInversion(notesHeld) {
         if(inv) {
             inversionElement.innerHTML = inversion(notesHeld);
         } else {
-            inversionElement.innerHTML = "";
+            inversionElement.innerHTML = "-";
         }
     } else {
-        inversionElement.innerHTML = "";
+        inversionElement.innerHTML = "-";
     }
 }
 
@@ -152,15 +174,38 @@ function onMIDIMessage(message) {
         notesHeld.splice(notesHeld.indexOf(data[1]), 1);
     }
 
+    notes_list = notesHeld.sort().map(midiNote).map(a => a.string);
+    if(notes_list.length == 0) {
+        notes_str = "-";
+    } else {
+        notes_str = notes_list.join(", ");
+    }
+
     notesElement = document.getElementById("notes");
-    notesElement.innerHTML = notesHeld.sort().map(midiNote).map(a => a.string);
+    notesElement.innerHTML = notes_str;
+
     detectInversion(notesHeld);
 }
 
-Soundfont.instrument(new AudioContext(), 'acoustic_grand_piano').then(function (piano) {
+const ac = new AudioContext();
+
+Soundfont.instrument(ac, 'acoustic_grand_piano').then(function (piano) {
     window.navigator.requestMIDIAccess().then(function (midiAccess) {
         midiAccess.inputs.forEach(function (midiInput) {
             piano.listenToMidi(midiInput)
         })
     })
 })
+
+document.getElementById("major_normal").addEventListener("click", function(event) {
+    Soundfont.instrument(ac, 'acoustic_grand_piano').then(function (clavinet) {
+    //clavinet.play('C4').stop(ac.currentTime + 0.5)
+    //clavinet.play('C4', ac.currentTime, { duration: 0.5})
+    clavinet.schedule(0,
+                      [{time: 0,   note: 'C3'},
+                       {time: 0.3, note: 'E3'},
+                       {time: 0.6, note: 'G3'},
+                       {time: 0.9, note: 'E3'},
+                       {time: 1.2, note: 'C3'}]);
+    });
+});
